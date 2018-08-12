d9a2391867e9cc8c9c84b3c3dd2be86a36a5299a
kalex19 <klewis@rockymountainprep.org>
2018-08-12 14:19:02 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -6,11 +6,9 @@ def recipe_ingredients(perfect_10_recipe)
   perfect_10_recipe
 end
 
-recipe_ingredients
 
-#2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
-def amount_of_chocolate_chips
-  return perfect_10_recipe |mini_dark_chocolate_chips, value|
+def amount_of_chocolate_chips(perfect_10_recipe)
+  {:mini_dark_chocolate_chips, value|
 # end
 
 amount_of_chocolate_chips
