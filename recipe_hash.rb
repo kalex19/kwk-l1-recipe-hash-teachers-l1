083651ff2333387de48dafb10a3132a30aebd8ee
kalex19 <klewis@rockymountainprep.org>
2018-08-12 14:16:43 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,5 +1,6 @@
 perfect_10_recipe = { :almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 teaspoon", :baking_powder => "1/2 teaspoon", :baking_soda => "1/4 teaspoon",:xanthan_gum => "1/2 teaspoon", :slivered_almonds => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 
+perfect_10_recipe
 
 def recipe_ingredients
   return perfect_10_recipe
